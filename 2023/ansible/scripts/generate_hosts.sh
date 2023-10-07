e6b8a5144463d1c0163f3dad4cd38a7febe6b7f8
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2023-10-08 02:31:02 +0900

    ansible

---
+++ 2023/ansible/scripts/generate_hosts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+cd ../../terraform
+
+ip_addresses=($(terraform output -json | jq -r '.ip_address.value[]'))
+server_names=($(terraform output -json | jq -r '.server_name.value[]'))
+
+cd $SCRIPT_DIR/..
+
+cat << EOF > hosts.yaml
+all:
+  children:
+    servers:
+      hosts:
+EOF
+
+for i in "${!ip_addresses[@]}"; do
+  echo "        ${server_names[i]}:" >> hosts.yaml
+  echo "          ansible_host: ${ip_addresses[i]}" >> hosts.yaml
+  echo "          ansible_ssh_private_key_file: ~/.ssh/id_ed25519" >> hosts.yaml
+done
